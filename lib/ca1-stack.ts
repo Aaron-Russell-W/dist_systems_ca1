8e1cdd7981f66f96118b2e69a3a29865c6c85ff9
aaron <aaronruss1@hotmail.com>
2023-11-06 22:32:27 +0000

    dynamodb created and seed file created

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -1,16 +1,42 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from '../shared/utils';
+import {moviereviews} from "../seed/moviereviews";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Ca1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ca1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the DynamoDB table
+    const table = new dynamodb.Table(this, 'MovieReviewsTable', {
+      partitionKey: {
+        name: 'MovieId',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      tableName:"MovieReviews",
+      removalPolicy:cdk.RemovalPolicy.DESTROY,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+    
+    
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [table.tableName]: generateBatch(moviereviews),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [table.tableArn],
+      }),
+    });
   }
 }
---
+++ seed/moviereviews.ts
@@ -0,0 +1,11 @@
+import { MovieReview } from "../shared/types";
+
+export const moviereviews : MovieReview[]=[
+    {
+        movieId:1234,
+        ReviewerNameIndex:'Jeff Murphy',
+        ReviewDate:'5/1/2023',
+        Content:"Finest Film Ever"
+    },
+    
+]
