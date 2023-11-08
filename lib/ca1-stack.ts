faabc0b5041c89d111ae59ecb18293bc3bd6afdb
aaron <aaronruss1@hotmail.com>
2023-11-08 19:36:10 +0000

    API for find by ID and Find all added to stack

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -6,6 +6,7 @@ import * as custom from "aws-cdk-lib/custom-resources";
 import { generateBatch } from '../shared/utils';
 import {moviereviews} from "../seed/moviereviews";
 import { Construct } from 'constructs';
+import * as apig from "aws-cdk-lib/aws-apigateway"
 
 export class Ca1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,7 +16,7 @@ export class Ca1Stack extends cdk.Stack {
     billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     partitionKey: { name: "movieId", type: dynamodb.AttributeType.NUMBER },
     removalPolicy: cdk.RemovalPolicy.DESTROY,
-    tableName: "Movies",
+    tableName: "MovieReviews",
     });
     
     
@@ -34,5 +35,64 @@ export class Ca1Stack extends cdk.Stack {
         resources: [movieReviewsTable.tableArn],
       }),
     });
+    const getMovieReviewByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieReviewByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getMovieReviewsById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieReviewsTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+      );
+      const getAllMovieReviewsFn = new lambdanode.NodejsFunction(
+        this,
+        "GetAllMovieReviewsFn",
+        {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/getAllMovieReviews.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: movieReviewsTable.tableName,
+            REGION: 'eu-west-1',
+          },
+        }
+        );
+
+        // Permissions
+        movieReviewsTable.grantReadData(getMovieReviewByIdFn)
+        movieReviewsTable.grantReadData(getAllMovieReviewsFn)
+        const api = new apig.RestApi(this, "RestAPI", {
+          description: "demo api",
+          deployOptions: {
+            stageName: "dev",
+          },
+          // 👇 enable CORS
+          defaultCorsPreflightOptions: {
+            allowHeaders: ["Content-Type", "X-Amz-Date"],
+            allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+            allowCredentials: true,
+            allowOrigins: ["*"],
+          },
+        });
+
+        const movieReviewsEndpoint = api.root.addResource("movies");
+        movieReviewsEndpoint.addMethod(
+          "GET",
+          new apig.LambdaIntegration(getAllMovieReviewsFn, { proxy: true })
+        );
+    
+        const movieReviewEndpoint = movieReviewsEndpoint.addResource("{movieId}");
+        movieReviewEndpoint.addMethod(
+          "GET",
+          new apig.LambdaIntegration(getMovieReviewByIdFn, { proxy: true })
+        );
   }
 }
